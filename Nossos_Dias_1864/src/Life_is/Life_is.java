88cfeeaf2dad9393735d23ccc55792b0c6bb4e7e
Débora De Grande <162713015+deboradsdg@users.noreply.github.com>
2025-11-23 21:30:39 -0300

    Delete Nossos_Dias_1864/src/Life_is directory

--- Nossos_Dias_1864/src/Life_is/Life_is.java
+++
@@ -1,9 +0,0 @@
-package Life_is;
-
-public class Life_is {
-	private static final String FRASE = "LIFE IS NOT A PROBLEM TO BE SOLVED";
-
-    public static String extrair(int n) {
-        return FRASE.substring(0, n);
-    }
-}
